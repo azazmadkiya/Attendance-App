afcc142c652c6b1e95b76f34293f6c61a016828d
azazmadkiya <74822032+azazmadkiya@users.noreply.github.com>
2026-09-06 14:44:21 +0530

    build: update application ID
    Change the application ID to differentiate the new build variant.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
   compileSdk { version = release(36) { minorApiLevel = 1 } }
 
   defaultConfig {
-    applicationId = "com.aistudio.haazripro.attendance"
+    applicationId = "com.aistudio.haazripro.attendance.new"
     minSdk = 24
     targetSdk = 36
     versionCode = 1
